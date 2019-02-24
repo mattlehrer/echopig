f6677f7eae072f9a3d685dec6a3b92292761c6cc
Matt Lehrer <matt@mattlehrer.com>
2019-02-24 13:08:49 -0800

    no user for feed

--- server/controllers/rssController.js
+++ server/controllers/rssController.js
@@ -10,69 +10,74 @@ module.exports = {
       if (err) {
         logger.error(`${err.name}: ${err.errmsg}`);
         next(err);
-      } else {
-        const { posts } = user;
-        // create an rss feed
-        // documentation: https://www.npmjs.com/package/podcast
-        const feed = new Podcast({
-          title: `${usernameForFeed}'s Favorites`,
-          description: `${usernameForFeed}'s feed of favorite podcast episodes powered by Echopig`,
-          feedUrl: `http://echopig.com/rss/${usernameForFeed}`,
-          siteUrl: `http://echopig.com/u/${usernameForFeed}`,
-          generator: 'Echopig.com',
-          // imageUrl: TODO: add avatar,
-          // itunesImage: TODO: add avatar,
-          // docs: TODO: 'https://www.echopig.com/rssDocs.html',
-          author: usernameForFeed,
-          language: 'en',
-          categories: ['Personal'],
-          itunesCategory: ['Personal'],
-          pubDate: posts.length > 0 ? posts[0].updatedAt : user.updatedAt,
-          ttl: '120',
-          itunesAuthor: usernameForFeed,
-          itunesSubtitle: `${usernameForFeed}'s feed of favorite podcast episodes powered by Echopig`,
-          itunesSummary: 'Create your own feed at http://www.echopig.com',
-          itunesOwner: {
-            name: usernameForFeed,
-            email: 'rss@echopig.com'
-          },
-          itunesExplicit: user.explicit
-        });
-        posts.forEach(post => {
-          feed.addItem({
-            title: post.episode.title,
-            description: post.episode.description,
-            url: `https://www.echopig.com/e/${post.episode.id}`,
-            guid: post.guid,
-            categories:
-              post.episode.podcast.genres !== null
-                ? post.episode.podcast.genres
-                : [],
-            author:
-              post.episode.podcast.author !== null
-                ? post.episode.podcast.author
-                : '',
-            enclosure: { url: post.episode.mp3URL },
-            date: post.updatedAt,
-            itunesAuthor:
-              post.episode.podcast.author !== null
-                ? post.episode.podcast.author
-                : '',
-            itunesExplicit:
-              post.episode.podcast.collectionExplicitness !== null
-                ? post.episode.podcast.collectionExplicitness
-                : false,
-            itunesSubtitle: post.comment !== null ? post.comment : '',
-            itunesDuration:
-              post.episode.duration !== null ? post.episode.duration : null
-          });
-        });
-
-        const xml = feed.buildXml();
-
-        res.set('Content-Type', 'application/rss+xml');
-        res.send(xml);
+        return;
       }
+      if (!user) {
+        res.status(404);
+        res.send('No such user');
+        return;
+      }
+      const { posts } = user;
+      // create an rss feed
+      // documentation: https://www.npmjs.com/package/podcast
+      const feed = new Podcast({
+        title: `${usernameForFeed}'s Favorites`,
+        description: `${usernameForFeed}'s feed of favorite podcast episodes powered by Echopig`,
+        feedUrl: `http://echopig.com/rss/${usernameForFeed}`,
+        siteUrl: `http://echopig.com/u/${usernameForFeed}`,
+        generator: 'Echopig.com',
+        // imageUrl: TODO: add avatar,
+        // itunesImage: TODO: add avatar,
+        // docs: TODO: 'https://www.echopig.com/rssDocs.html',
+        author: usernameForFeed,
+        language: 'en',
+        categories: ['Personal'],
+        itunesCategory: ['Personal'],
+        pubDate: posts.length > 0 ? posts[0].updatedAt : user.updatedAt,
+        ttl: '120',
+        itunesAuthor: usernameForFeed,
+        itunesSubtitle: `${usernameForFeed}'s feed of favorite podcast episodes powered by Echopig`,
+        itunesSummary: 'Create your own feed at http://www.echopig.com',
+        itunesOwner: {
+          name: usernameForFeed,
+          email: 'rss@echopig.com'
+        },
+        itunesExplicit: user.explicit
+      });
+      posts.forEach(post => {
+        feed.addItem({
+          title: post.episode.title,
+          description: post.episode.description,
+          url: `https://www.echopig.com/e/${post.episode.id}`,
+          guid: post.guid,
+          categories:
+            post.episode.podcast.genres !== null
+              ? post.episode.podcast.genres
+              : [],
+          author:
+            post.episode.podcast.author !== null
+              ? post.episode.podcast.author
+              : '',
+          enclosure: { url: post.episode.mp3URL },
+          date: post.updatedAt,
+          itunesAuthor:
+            post.episode.podcast.author !== null
+              ? post.episode.podcast.author
+              : '',
+          itunesExplicit:
+            post.episode.podcast.collectionExplicitness !== null
+              ? post.episode.podcast.collectionExplicitness
+              : false,
+          itunesSubtitle: post.comment !== null ? post.comment : '',
+          itunesDuration:
+            post.episode.duration !== null ? post.episode.duration : null
+        });
+      });
+
+      const xml = feed.buildXml();
+
+      res.set('Content-Type', 'application/rss+xml');
+      res.send(xml);
     });
   }
 };
