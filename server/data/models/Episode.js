c739248e34e05466d3309e93fe1806072c753150
Matt Lehrer <matt@mattlehrer.com>
2019-04-25 15:26:34 -0700

    add timestamps

--- server/data/models/Episode.js
+++ server/data/models/Episode.js
@@ -3,39 +3,44 @@ const autopopulate = require('mongoose-autopopulate');
 const shortid = require('shortid');
 
 module.exports.init = () => {
-  const episodeSchema = new mongoose.Schema({
-    _id: {
-      type: String,
-      default: shortid.generate
-    },
-    podcast: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Podcast',
-      autopopulate: true
-    },
-    title: String,
-    description: String,
-    image: String,
-    mp3URL: String,
-    releaseDate: Date,
-    shareURLs: [String],
-    posts: [
-      {
+  const episodeSchema = new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate
+      },
+      podcast: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Post'
-      }
-    ],
-    saves: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Save'
-      }
-    ],
-    duration: Number, // in seconds
-    parentalRating: String,
-    // ratingRiaa: Number,
-    listenNotesID: String
-  });
+        ref: 'Podcast',
+        autopopulate: true
+      },
+      title: String,
+      description: String,
+      image: String,
+      mp3URL: String,
+      releaseDate: Date,
+      shareURLs: [String],
+      posts: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Post'
+        }
+      ],
+      saves: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Save'
+        }
+      ],
+      duration: Number, // in seconds
+      parentalRating: String,
+      // ratingRiaa: Number,
+      listenNotesID: String
+    },
+    {
+      timestamps: true
+    }
+  );
 
   episodeSchema.plugin(autopopulate);
 
--- server/data/models/Podcast.js
+++ server/data/models/Podcast.js
@@ -1,26 +1,31 @@
 const mongoose = require('mongoose');
 
 module.exports.init = () => {
-  const podcastSchema = new mongoose.Schema({
-    iTunesID: { type: Number, alias: 'collectionId', unique: true },
-    author: { type: String, alias: 'artistName' },
-    title: { type: String, alias: 'collectionName' },
-    collectionViewUrl: { type: String, alias: 'iTunesURL' },
-    feedUrl: String,
-    artworkUrl30: String,
-    artworkUrl60: String,
-    artworkUrl100: String,
-    collectionExplicitness: String,
-    trackExplicitness: String,
-    country: String,
-    primaryGenreName: String,
-    contentAdvisoryRating: String,
-    artworkUrl600: String,
-    genreIds: [Number],
-    genres: [String],
-    listenNotesID: String,
-    appURLs: [String]
-  });
+  const podcastSchema = new mongoose.Schema(
+    {
+      iTunesID: { type: Number, alias: 'collectionId', unique: true },
+      author: { type: String, alias: 'artistName' },
+      title: { type: String, alias: 'collectionName' },
+      collectionViewUrl: { type: String, alias: 'iTunesURL' },
+      feedUrl: String,
+      artworkUrl30: String,
+      artworkUrl60: String,
+      artworkUrl100: String,
+      collectionExplicitness: String,
+      trackExplicitness: String,
+      country: String,
+      primaryGenreName: String,
+      contentAdvisoryRating: String,
+      artworkUrl600: String,
+      genreIds: [Number],
+      genres: [String],
+      listenNotesID: String,
+      appURLs: [String]
+    },
+    {
+      timestamps: true
+    }
+  );
 
   mongoose.model('Podcast', podcastSchema);
 };
